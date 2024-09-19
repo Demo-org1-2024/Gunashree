626ee0335f06c36e9eecaddfd9b6d9806615e9f4
Gunashree Purushotham <gunashreep@devtools.in>
2024-09-19 17:29:37 +0530

    Update SUB.java

--- SUB.java
+++ SUB.java
@@ -1,20 +1,9 @@
 public class SubtractTwoNumbers {
- 
     public static void main(String[] args) {
-         
-        // declare and initialize two integer numbers
         int x=50;
         int y=10;
- 
-        // declare another variable to hold the substracted value
-        int subtract;
- 
-        // calculate the substraction 
+        int subtract; 
         subtract = x - y;
- 
-        // print the calculated subtract value
         System.out.println("The substraction of two Numbers: " + subtract);
- 
-    }
- 
+    } 
 }
